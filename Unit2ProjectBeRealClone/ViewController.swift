91aff1684e38749d66205effcab34048a1fbf116
Raymond Chen <ahrchen@gmail.com>
2024-07-09 09:58:14 -0700

    Initial Commit

---
+++ Unit2ProjectBeRealClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unit2ProjectBeRealClone
+//
+//  Created by Raymond Chen on 7/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
