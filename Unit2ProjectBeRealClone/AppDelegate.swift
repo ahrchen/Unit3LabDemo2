29990400b911d24f6d0a5f35b00f29f34adcd153
Raymond Chen <ahrchen@gmail.com>
2024-07-11 00:09:05 -0700

    User is able to logout

--- Unit2ProjectBeRealClone/AppDelegate.swift
+++ Unit2ProjectBeRealClone/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ParseSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        ParseSwift.initialize(applicationId: "IZ8Oq5Qu8b7lmqQTfELevJBfigQaIuqku3iakxiq",
+                              clientKey: "hzWUzIAPencoIJXuhSPJButdepQpubahlfa2MITq",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
         return true
     }
 
--- Unit2ProjectBeRealClone/Feed/FeedViewController.swift
+++ Unit2ProjectBeRealClone/Feed/FeedViewController.swift
@@ -5,4 +5,51 @@
 //  Created by Raymond Chen on 7/9/24.
 //
 
-import Foundation
+
+import UIKit
+import ParseSwift
+
+class FeedViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func onLogOutTapped(_ sender: Any) {
+        showConfirmLogoutAlert()
+    }
+    
+    private func showConfirmLogoutAlert() {
+        let alertController = UIAlertController(title: "Log out of your account?", message: nil, preferredStyle: .alert)
+        let logOutAction = UIAlertAction(title: "Log out", style: .destructive) { _ in
+            NotificationCenter.default.post(name: Notification.Name("logout"), object: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(logOutAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+
+    private func showAlert(description: String? = nil) {
+        let alertController = UIAlertController(title: "Oops...", message: "\(description ?? "Please try again...")", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+}
+
+
+//extension FeedViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return posts.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as? PostCell else {
+//            return UITableViewCell()
+//        }
+//        cell.configure(with: posts[indexPath.row])
+//        return cell
+//    }
+//}
+
+extension FeedViewController: UITableViewDelegate { }
