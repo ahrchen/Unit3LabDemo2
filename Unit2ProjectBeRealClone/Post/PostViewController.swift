c18f02bf5cb7c9c4383cc6ad42c82bb85e515041
Raymond Chen <ahrchen@gmail.com>
2024-08-14 23:59:57 -0700

    Remove Take Picture Code

--- Unit2ProjectBeRealClone/Post/PostViewController.swift
+++ Unit2ProjectBeRealClone/Post/PostViewController.swift
@@ -160,29 +160,9 @@ class PostViewController: UIViewController {
     @IBAction func onTakePhotoTapped(_ sender: Any) {
         // Make sure the user's camera is available
         // NOTE: Camera only available on physical iOS device, not available on simulator.
-        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
-            print("❌📷 Camera not available")
-            return
-        }
-        locationButtonTapped()
-        // Instantiate the image picker
-        let imagePicker = UIImagePickerController()
-
-        // Shows the camera (vs the photo library)
-        imagePicker.sourceType = .camera
-
-        // Allows user to edit image within image picker flow (i.e. crop, etc.)
-        // If you don't want to allow editing, you can leave out this line as the default value of `allowsEditing` is false
-        imagePicker.allowsEditing = true
-
-        // The image picker (camera in this case) will return captured photos via it's delegate method to it's assigned delegate.
-        // Delegate assignee must conform and implement both `UIImagePickerControllerDelegate` and `UINavigationControllerDelegate`
-        imagePicker.delegate = self
         
-        // Present the image picker (camera)
-        present(imagePicker, animated: true)
-
     }
+    
     
     
     @IBAction func onViewTapped(_ sender: Any) {
